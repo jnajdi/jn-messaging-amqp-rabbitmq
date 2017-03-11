8e0771b32d321eb0060d61ea6e62a981ffa683c0
Jihane Najdi <jihane.najdi@vt.edu>
2017-03-11 07:36:05 -0800

    Added simple RabbitMq connection example.

--- src/main/java/jnajdi/sandbox/example/Application.java
+++ src/main/java/jnajdi/sandbox/example/Application.java
@@ -21,7 +21,15 @@ public class Application {
 	public final static String exchangeName = "jn-messaging-amqp-exchange";
 	
 	public static void main(String[] args) {
+//		ConnectionFactory connectionFactory = new CachingConnectionFactory();
+//		AmqpAdmin admin = new RabbitAdmin(connectionFactory);
+//		admin.declareQueue(new Queue("myqueue"));
+//		AmqpTemplate template = new RabbitTemplate(connectionFactory);
+//		template.convertAndSend("myqueue", "foo");
+//		String foo = (String) template.receiveAndConvert("myqueue");
+		
 		SpringApplication.run(Application.class, args);
+		
 	}
 	
 	@Bean 
@@ -54,6 +62,7 @@ public class Application {
 		
 		return container;
 	}
+	
 	
 	
 }
